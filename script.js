b38f347ed8e6251430be4ec1d6fb368de83fde7a
otabekov1ch <88477479+otabekov1ch@users.noreply.github.com>
2022-05-17 01:51:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const firstName = prompt("Ismining")
+const lastName = prompt("Familiyangiz")
+const age = prompt("Tug'ilgan yilingiz?")
+const result = firstName + ' ' + lastName + ' ' + age;
+alert(result);
+if (age >= 2004) {
+    alert('Armiya adeni uyimas hali yoshsan')
+} else {
+    alert('Armiyaga borasiz bro')
+};
